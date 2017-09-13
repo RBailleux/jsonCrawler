bc66f53e57d288a2749cd15d20be9f2e92fe90b5
robin <robin@Robin-Workstation>
2017-09-13 14:32:44 +0200

    js

--- jsonCrawler.js
+++ jsonCrawler.js
@@ -2,6 +2,7 @@ function readJSON(file) {
     var request = new XMLHttpRequest();
     request.open('GET', file, false);
     request.send(null);
-    if (request.status == 200)
-        return request.responseText;
+    if (request.status == 200){
+        alert(request.responseText);
+    }
 };
